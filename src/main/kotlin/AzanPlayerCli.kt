246502a86af98366d53e738afccb52088c3c7334
Ismail <i.mohamed@uq.edu.au>
2021-05-28 11:40:16 +1000

    using latest version of kotlin-azan. added more adhan to play from

--- src/main/kotlin/AzanPlayerCli.kt
+++ src/main/kotlin/AzanPlayerCli.kt
@@ -7,7 +7,51 @@ import kotlin.concurrent.timerTask
 val salatNamesToPlay = listOf(SalatNames.FAJR, SalatNames.DHUHR, SalatNames.ASR, SalatNames.MAGHRIB, SalatNames.ISHA)
     .map { it.niceName }
 
+val adhanFiles = listOf(
+    "AdhanMakkah.wav",
+    "AdhanMedina.wav",
+    "AdhanAlaqsa.wav",
+    "AdhanEgypt.wav",
+)
+val adhanFajrFile = "AdhanFajr.wav"
+val beepFile = "beep.wav"
+
+lateinit var adhan: List<Music>
+lateinit var adhanFajr: Music
+lateinit var beep: Music
+
+val r = Random()
+
+fun getAdhan() : Music {
+    val i = r.nextInt() % adhan.size
+    println("Selected adhan: ${adhanFiles[i]}")
+    return adhan[i]
+}
+
+fun getAdhan(salatNiceName: String) : Music =
+    when (salatNiceName) {
+        !in salatNamesToPlay -> {
+            println("Selected beep")
+            beep
+        }
+        SalatNames.FAJR.niceName -> {
+            println("Selected adhan fajr")
+            adhanFajr
+        }
+        else -> getAdhan()
+    }
+
+fun playAdhan(salatNiceName: String) {
+    val adhanToPlay = getAdhan(salatNiceName)
+    adhanToPlay.rewind()
+    adhanToPlay.play(false)
+}
+
 fun main() {
+    TinySound.init()
+    adhan = adhanFiles.map(TinySound::loadMusic)
+    adhanFajr = TinySound.loadMusic(adhanFajrFile)
+
     val location = Location(
         "Brisbane",
         -27.468968,
@@ -25,28 +69,25 @@ fun main() {
 
     val timer = Timer("Azan Player Cli", false)
 
-    TinySound.init()
-    val adhan = TinySound.loadMusic("AdhanMedina.wav")
-
-    scheduleNextAzan(salatTimes, timer, adhan)
+    scheduleNextAzan(salatTimes, timer)
 }
 
-fun scheduleNextAzan(salatTimes: SalatTimes, timer: Timer, adhan: Music) {
+fun scheduleNextAzan(salatTimes: SalatTimes, timer: Timer) {
     println("Scheduling salat: ${salatTimes.nextSalatTime()}")
     println("in: ${salatTimes.secondsToNextSalat()} seconds")
     println("-------------------------------")
 
     timer.schedule(
         timerTask {
+            val currentSalat = salatTimes.currentSalatTime()
             println("It's azan time!")
-            println("Salat: ${salatTimes.currentSalatTime()}")
-            if (salatTimes.currentSalatTime().first in salatNamesToPlay) {
-                adhan.rewind()
-                adhan.play(false)
-            }
+            println("Salat: $currentSalat")
+
+            playAdhan(currentSalat.first)
+
             println("-------------------------------")
 
-            scheduleNextAzan(SalatTimes(location = salatTimes.location), timer, adhan)
+            scheduleNextAzan(SalatTimes(location = salatTimes.location), timer)
 
         }, salatTimes.secondsToNextSalat() * 1000
     )
